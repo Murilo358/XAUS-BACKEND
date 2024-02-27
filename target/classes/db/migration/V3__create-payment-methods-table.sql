88b8112fd3e17fd74a5802fa07b6bc783b166975
Murilo Barbosa <111542827+Murilo358@users.noreply.github.com>
2024-02-27 16:40:30 -0300

    Delete target directory

--- target/classes/db/migration/V3__create-payment-methods-table.sql
+++
@@ -1,6 +0,0 @@
-create table
-  public.payment_methods (
-    id bigserial,
-    payment_method character varying(255) null,
-    constraint payment_methods_pkey primary key (id)
-  ) tablespace pg_default;
--- target/classes/db/migration/V6__create-not-registred-client.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO clients (email, cpf, name) values ('UsuárioNãoCadastrado@XAUS.com', 'Usuário não cadastrado', 'Usuário não cadastrado');
-
